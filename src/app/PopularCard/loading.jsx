55285214ec0666f843753af658bb7f945c3936c5
MD Ibna Abid <mdabout2@gmail.com>
2026-05-20 11:14:41 +0600

    add loader

---
+++ src/app/PopularCard/loading.jsx
@@ -0,0 +1,12 @@
+
+
+const loading = () => {
+    return (
+        <div>
+            <span className="loading text-center mx-auto mt-6 flex items-center loading-dots loading-xl"></span>
+            
+        </div>
+    );
+};
+
+export default loading;
